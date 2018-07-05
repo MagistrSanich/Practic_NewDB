0f141e28b919048863bab9a2275481cc3d80f08d
MagistrSanich <iamsexysexysexy@gmail.com>
2018-07-05 13:42:33 +0700

    Довел до ума

--- Practic_NewDB/Controllers/HomeController.cs
+++ Practic_NewDB/Controllers/HomeController.cs
@@ -16,6 +16,60 @@ namespace Practic_NewDB.Controllers
             var players = db.Players.Include(p => p.Team);
             return View(players.ToList());
         }
+        public ActionResult TeamDetails(int? Id)
+        {
+            if(Id == null)
+            {
+                return HttpNotFound();
+            }
+            Team team = db.Teams.Include(t => t.Players).FirstOrDefault(t => t.Id==Id);
+            //FirstOrDefault - возвращает первый элемент последовательности удовлетворяющий условию, либо 
+            //возвращает дефолтный эл.
+            if (team == null)
+            {
+                return HttpNotFound();
+            }
+            return View(team);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            SelectList teams = new SelectList(db.Teams, "Id", "Name");
+            ViewBag.Teams = teams;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Player player)
+        {
+            db.Players.Add(player);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if(id==null)
+            {
+                return HttpNotFound();
+            }
+            Player player = db.Players.Find(id);
+            if(player!= null)
+            {
+                SelectList selects = new SelectList(db.Teams, "Id", "Name", player.TeamId);
+                ViewBag.Teams = selects;
+                return View(player);
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult Edit(Player player)
+        {
+            db.Entry(player).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
         public ActionResult About()
         {
